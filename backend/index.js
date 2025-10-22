f431422666a419a5c0d633bcfb452a8871eb7884
ShivaGupta-14 <shivascalerite@gmail.com>
2025-10-22 23:08:27 +0530

    setuping db, creating User Model

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 import express from "express"
 import Dotenv from "dotenv"
+import connectdb from "./config/db.js";
 Dotenv.config();
-let port = process.env.PORT || 6000
+let port = process.env.PORT || 6000;
 
 const app = express()
 
@@ -9,5 +10,6 @@ app.get("/",(req,res)=>{
     res.send("Hi")
 })
 app.listen(port,()=>{
+    connectdb();
     console.log(`Server is running at Port: ${port}.`)
 })
