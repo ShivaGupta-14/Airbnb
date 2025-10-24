b175f9d0f6943041127e28ab8ffb34b307935fce
Shiva Gupta <shivascalerite@gmail.com>
2025-10-24 19:42:58 +0530

    Remove trailing slash from server URL

--- frontend/src/Context/AuthContext.jsx
+++ frontend/src/Context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState } from 'react'
 export const authDataContext = createContext()
 function AuthContext({children}) {
-    const serverUrl = "https://airbnb-2xpb.onrender.com/"
+    const serverUrl = "https://airbnb-2xpb.onrender.com"
 
     let [loading,setLoading]=useState(false)
 
